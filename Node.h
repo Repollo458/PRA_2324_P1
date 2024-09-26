6be5985c385d4ba2433e08cee85d5e8d1dbd1864
Jorge <jorgerepollo458@gmail.com>
2024-09-26 19:37:54 +0200

    Añadida implementacion de la clase Node

---
+++ Node.h
@@ -0,0 +1,9 @@
+#include <ostream>
+
+template <typename T>
+
+class Node{
+	public:
+		Node(T data, Node<T>* next=nullptr);
+		friend std::ostream& operator<<(std::ostream &out, const Node<T> &node);
+};
